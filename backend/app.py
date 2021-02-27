5c015cfcd5ea8281d1b7aa7ee3ae042b5e49f984
Julian ranieri <jumpingafterrain@gmail.com>
2021-02-27 15:19:27 -0800

    gunicorn host

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 import time
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../build", static_url_path='/')
 if __name__ == "__main__":
   app.run(debug=True)
+
+@app.route('/')
+def index():
+  return app.send_static_file('index.html')
 
 @app.route('/api/time')
 def get_current_time():
--- src/App.js
+++ src/App.js
@@ -19,17 +19,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
           the current time is {currentTime}.
         </p>
       </header>
