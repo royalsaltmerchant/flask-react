314cb78efaa8751f1c30f2b78f40c17050944fea
Julian ranieri <jumpingafterrain@gmail.com>
2021-02-27 15:04:54 -0800

    working call to backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, { useState, useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [currentTime, setCurrentTime] = useState(0)
+
+  useEffect(() => {
+    fetch('/api/time')
+    .then(res => res.json())
+    .then(data => {
+      setCurrentTime(data.time)
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,7 +30,7 @@ function App() {
           Learn React
         </a>
         <p>
-          test
+          the current time is {currentTime}.
         </p>
       </header>
     </div>
